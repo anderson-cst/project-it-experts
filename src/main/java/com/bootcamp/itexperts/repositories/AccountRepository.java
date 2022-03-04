2ed2fc83185b1707dd62c233b68795dd577dc86d
anderson-cst <andersonleandrocosta@hotmail.com>
2022-03-04 15:31:05 -0300

    update id Long to Integer

--- src/main/java/com/bootcamp/itexperts/repositories/AccountRepository.java
+++ src/main/java/com/bootcamp/itexperts/repositories/AccountRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.bootcamp.itexperts.models.AccountModel;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountModel, Long>{
+public interface AccountRepository extends JpaRepository<AccountModel, Integer>{
 	
 }

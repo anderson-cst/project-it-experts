ac0592efca1108d7b553a6c7a3a2e6d1dfd01a11
anderson-cst <andersonleandrocosta@hotmail.com>
2022-03-04 15:24:08 -0300

    add unique constraints names and constructor

--- src/main/java/com/bootcamp/itexperts/models/AccountModel.java
+++ src/main/java/com/bootcamp/itexperts/models/AccountModel.java
@@ -1,40 +1,66 @@
 package com.bootcamp.itexperts.models;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-@Table(name="tb_accounts")
+@Table(name="tb_accounts", uniqueConstraints= {
+		@UniqueConstraint(name="unique_account_code", columnNames= {"account_code"}),
+		@UniqueConstraint(name="unique_register_id", columnNames= {"register_id"})})
 public class AccountModel implements Serializable{
 		
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Column(name = "nameOwner", nullable = false, length = 50)
+	private Integer id;
+	@Column(name = "name_owner", nullable = false, length = 50)
 	private String nameOwner;
-	@Column(name = "agencyCode", nullable = false, length = 5)
+	@Column(name = "agency_code", nullable = false, length = 5)
 	private String agencyCode;
-	@Column(name = "accountCode", nullable = false, unique = true, length = 10)
+	@Column(name = "account_code", nullable = false, length = 10)
 	private String accountCode;
-	@Column(name = "verificationDigital", nullable = false, length = 1)
+	@Column(name = "verification_digital", nullable = false, length = 1)
 	private String verificationDigital;
-	@Column(name = "registerId", nullable = false, unique = true, length = 20)
+	@Column(name = "register_id", nullable = false, length = 20)
 	private String registerId;
+	@OneToMany(mappedBy = "accountModelId", cascade = CascadeType.ALL) //fetch = FetchType.LAZY
+	//@JsonManagedReference
+	private List<CardModel> cardModel;
 	
 	
+		
+	public AccountModel() {
+		super();
+	}
+
+	public AccountModel(Integer id, String nameOwner, String agencyCode, String accountCode, String verificationDigital,
+			String registerId, List<CardModel> cardModel) {
+		this.id = id;
+		this.nameOwner = nameOwner;
+		this.agencyCode = agencyCode;
+		this.accountCode = accountCode;
+		this.verificationDigital = verificationDigital;
+		this.registerId = registerId;
+		this.cardModel = cardModel;
+	}
 	
-	public Long getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(Long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	public String getNameOwner() {
@@ -67,7 +93,12 @@ public class AccountModel implements Serializable{
 	public void setRegisterId(String registerId) {
 		this.registerId = registerId;
 	}
-	
-	
+	@JsonManagedReference
+	public List<CardModel> getCardModel() {
+		return cardModel;
+	}
+	public void setCardModel(List<CardModel> cardModel) {
+		this.cardModel = cardModel;
+	}
 
 }
